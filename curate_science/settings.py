b8cd588f4fed78e181a411c3f323709700901716
Alex Kyllo <alex.kyllo@gmail.com>
2018-09-19 00:26:22 -0700

    change debug back to False

--- curate_science/settings.py
+++ curate_science/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #False
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
