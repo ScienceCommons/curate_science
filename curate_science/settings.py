82c38d024a7bb1db49cbdf2915924e2b62352b67
Alex Kyllo <alex.kyllo@gmail.com>
2018-11-18 22:21:52 -0800

    s/GCP_PROJECT/GOOGLE_CLOUD_PROJECT/

--- curate_science/settings.py
+++ curate_science/settings.py
@@ -16,12 +16,12 @@ load_dotenv()
 
 # Google App Engine sets environment variable GOOGLE_CLOUD_PROJECT
 # If this env var exists, then the app is running on GAE. Else it's local dev environment.
-if os.getenv('GCP_PROJECT'):
+if os.getenv('GOOGLE_CLOUD_PROJECT'):
     #DB_HOST='/cloudsql/curate-science-216207:europe-west1:curatedb'
     DEBUG = False
     DB_HOST='35.205.158.247' #curatevm
 
-    if os.getenv('GCP_PROJECT') == 'curatescience-staging':
+    if os.getenv('GOOGLE_CLOUD_PROJECT') == 'curatescience-staging':
         DB_NAME = 'curate_staging'
     else:
         DB_NAME = 'curate'
