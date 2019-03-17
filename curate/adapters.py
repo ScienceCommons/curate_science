91e505910a0a2de1a6795c023828afe8d289c02e
Alex Kyllo <alex.kyllo@gmail.com>
2019-03-16 22:53:33 -0700

    make signup closed for non-invited users again

--- curate/adapters.py
+++ curate/adapters.py
@@ -1,10 +1,22 @@
 from invitations.models import Invitation
 from curate.models import UserProfile
+from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
 
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
-        return False
+        if hasattr(request, 'session') and request.session.get(
+                'account_verified_email'):
+            return True
+        elif settings.INVITATIONS_INVITATION_ONLY is True:
+            # Site is ONLY open for invites
+            return False
+        else:
+            # Site is open to signup
+            return True
+
+    def get_user_signed_up_signal(self):
+        return user_signed_up
 
     def save_user(self, request, user, form):
         data = form.cleaned_data
@@ -25,7 +37,6 @@ class AccountAdapter(DefaultAccountAdapter):
             profile.save()
             #import pdb; pdb.set_trace()
         except:
-            user.save()
             profile = UserProfile.objects.create(user=user)
             profile.save()
             user.username = profile.slug
