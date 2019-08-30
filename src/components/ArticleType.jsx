db029c9811350b4875f7133ca552350a11dce90a
Mick O'Brien <mickaobrien@gmail.com>
2019-08-30 15:56:33 +0100

    Only add titles to article type tags that don't have popovers to avoid material-ui errors

--- src/components/ArticleType.jsx
+++ src/components/ArticleType.jsx
@@ -10,51 +10,54 @@ class ArticleType extends React.Component {
         super(props);
     }
 
-	render_article_type_label() {
-		let { article, label_styles, type, replication_data } = this.props
-		let at = find(C.ARTICLE_TYPES, {id: type.toUpperCase()})
-		let count
-		let label = at != null ? at.label : "Unknown"
-		let color = at != null ? at.color || '#000000' : "#000000"
-		const st = {
-			color: color,
-			border: `1px solid ${color}`
-		}
-		if (type == 'REPLICATION' && !isEmpty(replication_data)) {
-			count = <span className="Count">{replication_data.number_of_reps}</span>
-		}
+  render_article_type_label() {
+    let { article, label_styles, type, replication_data } = this.props
+    let at = find(C.ARTICLE_TYPES, {id: type.toUpperCase()})
+    let count
+    let label = at != null ? at.label : "Unknown"
+    let color = at != null ? at.color || '#000000' : "#000000"
+    const st = {
+      color: color,
+      border: `1px solid ${color}`
+    }
+    if (type == 'REPLICATION' && !isEmpty(replication_data)) {
+      count = <span className="Count">{replication_data.number_of_reps}</span>
+    }
 
-		let type_label = (
-			<span className="ArticleBadgeWithCount ArticleType" style={{...st, ...label_styles}} title={at.description}>
-				{ label }{ count }
-			</span>
-		)
+    function render_type_label(title) {
+      return (
+        <span className="ArticleBadgeWithCount ArticleType" style={{...st, ...label_styles}} title={title}>
+            { label }{ count }
+        </span>
+      )
+    }
+
     const has_reproducibility_data = article.reproducibility_original_study && article.reproducibility_original_study_url
 
-		if (type == 'REPLICATION' && !isEmpty(replication_data)) {
-			return (
-				<MouseOverPopover target={type_label} key='rep_popover'>
-					<div style={{padding: 10}}>
-						<Typography variant="body1">Article reports { replication_data.number_of_reps } replications of <a href={replication_data.original_article_url} target="_blank">{ replication_data.original_study }</a> <em>{ replication_data.target_effects || "" }</em></Typography>
-						<Typography variant="body2" style={{marginTop: 15, color: "#808080"}}>
-							A replication is a study that uses a methodology that is
-							'close' or 'very close' to a previous study (see <a href="/sitestatic/legacy/logos/replication-taxonomy-v4_small.png" target="_blank">replication taxonomy</a> for details).
-						</Typography>
-					</div>
-				</MouseOverPopover>
-			)
-		} else if (type === 'REPRODUCIBILITY' && has_reproducibility_data) {
-			return (
-				<MouseOverPopover target={type_label} key='rep_popover'>
-					<div style={{padding: 10}}>
+    if (type == 'REPLICATION' && !isEmpty(replication_data)) {
+      return (
+        <MouseOverPopover target={render_type_label()} key='rep_popover'>
+          <div style={{padding: 10}}>
+            <Typography variant="body1">Article reports { replication_data.number_of_reps } replications of <a href={replication_data.original_article_url} target="_blank">{ replication_data.original_study }</a> <em>{ replication_data.target_effects || "" }</em></Typography>
+            <Typography variant="body2" style={{marginTop: 15, color: "#808080"}}>
+                A replication is a study that uses a methodology that is
+                'close' or 'very close' to a previous study (see <a href="/sitestatic/legacy/logos/replication-taxonomy-v4_small.png" target="_blank">replication taxonomy</a> for details).
+              </Typography>
+            </div>
+          </MouseOverPopover>
+      )
+    } else if (type === 'REPRODUCIBILITY' && has_reproducibility_data) {
+      return (
+        <MouseOverPopover target={render_type_label()} key='rep_popover'>
+          <div style={{padding: 10}}>
             <Typography variant="body1">
-              Article reports a reproducibility/robustness reanalysis of <a href={article.reproducibility_original_study_url}>{article.reproducibility_original_study}</a>.
-            </Typography>
-					</div>
-				</MouseOverPopover>
-			)
-		} else return type_label
-	}
+                Article reports a reproducibility/robustness reanalysis of <a href={article.reproducibility_original_study_url}>{article.reproducibility_original_study}</a>.
+              </Typography>
+            </div>
+          </MouseOverPopover>
+      )
+    } else return render_type_label(at.description)
+  }
 
 	render() {
 		let {registered_report} = this.props
