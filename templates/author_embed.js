98f25d1d63c2cc20706df0619f9ff088afdcdcf5
Mick O'Brien <mickaobrien@gmail.com>
2019-10-21 15:50:35 +0100

    Update author embed script to calculate the iframe src path dynamically based on where the script is hosted

--- templates/author_embed.js
+++ templates/author_embed.js
@@ -1,7 +1,30 @@
+function getLocation(href) {
+  var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+  return match && {
+    href: href,
+    protocol: match[1],
+    host: match[2],
+    hostname: match[3],
+    port: match[4],
+    pathname: match[5],
+    search: match[6],
+    hash: match[7]
+  }
+}
+
+function authorURL(slug) {
+  var href = document.currentScript.src;
+  var location = getLocation(href);
+  var port = location.port ? ':' + location.port : '';
+  return location.protocol + '//' + location.hostname + port + '/app/author-embed/' + slug
+}
+
+
 var iframe = document.createElement('iframe');
-iframe.setAttribute('src', 'http://localhost:8000/app/author-embed/{{ slug }}');
+
+iframe.setAttribute('src', authorURL('{{ slug }}'));
 iframe.setAttribute('width', '100%');
 iframe.setAttribute('height', '1000px');
 
-var thisScript = document.currentScript
+var thisScript = document.currentScript;
 thisScript.parentNode.insertBefore(iframe, thisScript.nextSibling);
