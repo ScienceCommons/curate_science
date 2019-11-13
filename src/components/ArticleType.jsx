282d9c3f8676b871eb4a0c2dccf9dd625cb9efbc
JoeAmmar <33490831+JoeAmmar@users.noreply.github.com>
2019-11-13 14:28:28 -0500

    Small naming/file directory/text related changes
    updated text "Advisory Board (as of April 2019)" to "Advisory Board"
    
    updated current replication taxonomy infographic (did not delete old files, created new directors and updated code to reflect changes)
    
    updated replication definition text to "A study is considered a replication if it uses a sufficiently similar methodology relative to a previous study (i.e., a 'Close', 'Very close', or 'Exact' replication according to [our replication taxonomy])."
    
    updated overall site title to "Transparent and credible evidence."
    
    changed "Prereg. prot. URLs" to "Preregistration protocol URLs" in ArticleEditor
    
    updated signup page text to: "We're in an early beta phase, hence sign up is currently limited to a small group of highly motivated researchers who are leading the transparency and replication movement in science. If you are such a researcher, please email us at curatescience@gmail.com and we will promptly invite you to the platform (you can also sign up to our newsletter to stay updated about our public beta launch)."

--- src/components/ArticleType.jsx
+++ src/components/ArticleType.jsx
@@ -41,8 +41,7 @@ class ArticleType extends React.Component {
           <div style={{padding: 10}}>
             <Typography variant="body1">Article reports { replication_data.number_of_reps } replications of <a href={replication_data.original_article_url} target="_blank">{ replication_data.original_study }</a> <em>{ replication_data.target_effects || "" }</em></Typography>
             <Typography variant="body2" style={{marginTop: 15, color: "#808080"}}>
-                A replication is a study that uses a methodology that is
-                'close' or 'very close' to a previous study (see <a href="/sitestatic/legacy/logos/replication-taxonomy-v4_small.png" target="_blank">replication taxonomy</a> for details).
+                A study is considered a replication if it uses a sufficiently similar methodology relative to a previous study (i.e., a 'Close', 'Very close', or 'Exact' replication according to our <a href="/sitestatic/infographics/replication-taxonomy-v0.4.0.png" target="_blank">replication taxonomy</a>.
               </Typography>
             </div>
           </MouseOverPopover>
