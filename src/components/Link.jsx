75b067b2b0fef05fb4be6cf3f375580cf93eab49
Mick O'Brien <mickaobrien@gmail.com>
2019-11-22 16:19:52 +0000

    Pass all props to custom Link component

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,11 +4,12 @@ import { HashLink } from 'react-router-hash-link';
 import { TOPBAR_HEIGHT } from './TopBar.jsx';
 
 
-export function Link({ to, children }) {
+export function Link({ to, children, ...props }) {
     return (
         <HashLink
             to={to}
             scroll={el => window.scroll(0, el.offsetTop - TOPBAR_HEIGHT)}
+            {...props}
         >
             {children}
         </HashLink>
