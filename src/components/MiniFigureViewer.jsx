e7451fb824e213bb9795f3f71e36d192de8e4f10
Mick O'Brien <mickaobrien@gmail.com>
2020-02-21 20:01:14 +0000

    Add lazy loading to the MiniFigureViewer
    Initial the first image is loaded and the one's before and after it. Then whenever a new image is swiped to, the images before and after that are loaded.

--- src/components/MiniFigureViewer.jsx
+++ src/components/MiniFigureViewer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Carousel } from 'react-responsive-carousel';
 
 import { FancyBoxViewer } from 'util/fancybox'
@@ -19,6 +19,35 @@ export default function MiniFigureViewer({ figures }) {
 
   const multipleFigures = figures.length > 0
 
+  const onChange = function(visibleIndex) {
+    markFiguresVisible(visibleIndex)
+  }
+
+  // Track which figures should be rendered to a allow lazy-loading of images
+  // If a figure's index isn't in the visibleFigures set, the image isn't loaded
+  // We add the first image to the set by default
+  const [visibleFigures, setVisibleFigures] = useState(new Set([0]))
+
+  const markFiguresVisible = function(index) {
+    // Add the figures before and after the current index to the set of visible figures
+    if (index > figures.length - 1 || index < 0) return
+
+    const nextIndex = (index + 1) % figures.length
+    const prevIndex = (index - 1 + figures.length) % figures.length
+
+    visibleFigures.add(index)
+    visibleFigures.add(prevIndex)
+    visibleFigures.add(nextIndex)
+
+    // We have to create a new Set here to force React to rerender
+    setVisibleFigures(new Set(visibleFigures))
+  }
+
+  useEffect(() => {
+    // Load the figures before and after the first image initially
+    markFiguresVisible(0)
+  }, [])
+
   return (
     <div style={{ height: 80, width: 80 }}>
       <Carousel
@@ -29,6 +58,7 @@ export default function MiniFigureViewer({ figures }) {
         showArrows={multipleFigures}
         showIndicators={false}
         className="MiniFigureViewer"
+        onChange={onChange}
       >
         {
           figures.map((figure, index) => {
@@ -40,7 +70,10 @@ export default function MiniFigureViewer({ figures }) {
                 onClick={expand}
                 data-index={index}
               >
-                <img src={figure.image} style={{ height: 80, width: 80, objectFit: 'cover' }}/>
+                <img 
+                  src={visibleFigures.has(index) ? figure.image : null}
+                  style={{ height: 80, width: 80, objectFit: 'cover' }}
+                />
               </a>
             )
           })
