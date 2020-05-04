fa28284b7c803d79be46b22c98847de0ef0e5a08
Dominik Lenda <dlenda1729@gmail.com>
2020-05-04 22:49:40 +0200

    Reduce minimum viewport breakpoint from 1920px to 1500px

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,15 @@ const theme = createMuiTheme({
             fontSize: 13
         }
     }
+  },
+  breakpoints: {
+    values: {
+        xs: 0,
+        sm: 450,
+        md: 600,
+        lg: 1200,
+        xl: 1500
+    }
   }
 });
 
